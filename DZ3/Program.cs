dfb1400206eb63399bf040a3b8c09eac9910fa94
Mihail <Dante9041@mail.ru>
2022-06-10 10:34:49 +0300

    add DZ3

---
+++ DZ3/Program.cs
@@ -0,0 +1,20 @@
+﻿int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = Input(" номер дня недели: ");
+
+if (number == 6 || number == 7)
+    {
+    Console.WriteLine("да");
+    }
+else if (number >7)
+    {
+    Console.WriteLine("не правильное число");
+    }
+else
+    {
+    Console.WriteLine("нет");
+    }
